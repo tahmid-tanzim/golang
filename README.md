1fe39e2cbd5b546f0aacdaa292c6d33388664c37
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-10-02 15:27:11 +0600

    Declare and initialize variables

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # The Go Programming Language
 
-### Go installed in `cd /usr/local/go/`
-### Go commands `go`
+### Install Go on macOS
+[https://golang.org/dl/](https://golang.org/dl/)
+
+### Go installed in `cd /usr/local/go/bin/`
+### Go version `go version`
 ### Go compile & run `go run index.go`
--- index.go
+++ index.go
@@ -5,7 +5,27 @@ import (
   "strings"
 )
 
+const MAX_SIZE int = 100
+
 func main() {
-  fmt.Println("Hello World from Go!")
-  fmt.Println(strings.ToUpper("Tahmid Tanzim Lupin")) 
+  fmt.Println(strings.ToUpper("Hello World from Go!")) 
+
+  // Declare and initialize variables  
+  var fullName string = "Tahmid Tanzim Lupin"
+  fmt.Println(fullName)
+  fmt.Printf("variable type = %T\n", fullName)
+
+  var defaultInt int
+  fmt.Println(defaultInt)
+  fmt.Printf("variable type = %T\n", defaultInt)
+
+  var age int = 26
+  fmt.Println(age)
+  fmt.Printf("variable type = %T\n", age)
+
+  sum := 0
+  for i := 0; i < 10; i++ {
+    sum += i
+  }
+  fmt.Println(sum)
 }
