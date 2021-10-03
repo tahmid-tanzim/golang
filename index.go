36bd4f54dff23a1652326bb2a1d1e42f8b6f1732
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-10-03 10:32:19 +0600

    Use the math package

--- index.go
+++ index.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-  "fmt"
-  "strings"
-  "strconv"
-  "bufio"
-  "os"
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	// "math"
 )
 
 const MAX_SIZE int = 100
@@ -40,6 +42,26 @@ func main() {
   } else {
     fmt.Println("String to Float - ", validConvertedData)
   }
+
+  // 4. Convert types before using
+  var a int = 8
+  var b float64 = 42.75
+  ab0 := float64(a) + b
+  ab1 := a + int(b)
+  fmt.Printf("(float64) Total - %v, Type - %T\n", ab0, ab0)
+  fmt.Printf("(int) Total - %v, Type - %T\n", ab1, ab1)
+
+  // 5. Use the math package
+  c1, c2, c3 := 23.5, 65.1, 76.3
+  c0 := c1 + c2 + c3
+  fmt.Println(c0)
+
+  c0 = math.Round(c0 * 100) / 100
+  fmt.Println(c0)
+
+  circleRadius := 15.5
+  circumference := circleRadius * 2 * math.Pi
+  fmt.Printf("Circumference - %.2f\n", circumference)
 
   sum := 0
   for i := 0; i < 10; i++ {
