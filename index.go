96e8906743c8b3361447a920ae15d1a917905853
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-10-04 11:10:01 +0600

    Store unordered values in maps

--- index.go
+++ index.go
@@ -15,106 +15,118 @@ import (
 const MAX_SIZE int = 100
 
 func main() {
-  fmt.Println(strings.ToUpper("Hello World from Go!")) 
+	fmt.Println(strings.ToUpper("Hello World from Go!"))
 
-  // 1. Declare and initialize variables  
-  var fullName string = "Tahmid Tanzim Lupin"
-  fmt.Println(fullName)
-  fmt.Printf("variable type = %T\n", fullName)
+	// 1. Declare and initialize variables
+	var fullName string = "Tahmid Tanzim Lupin"
+	fmt.Println(fullName)
+	fmt.Printf("variable type = %T\n", fullName)
 
-  var defaultInt int
-  fmt.Println(defaultInt)
-  fmt.Printf("variable type = %T\n", defaultInt)
+	var defaultInt int
+	fmt.Println(defaultInt)
+	fmt.Printf("variable type = %T\n", defaultInt)
 
-  var age int = 26
-  fmt.Println(age)
-  fmt.Printf("variable type = %T\n", age)
+	var age int = 26
+	fmt.Println(age)
+	fmt.Printf("variable type = %T\n", age)
 
-  // 2. Get input from Console
-  reader := bufio.NewReader(os.Stdin)
-  fmt.Print("Enter text - ")
-  input, _ := reader.ReadString('\n')
-  fmt.Println("You have entered - ", input)
+	// 2. Get input from Console
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text - ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You have entered - ", input)
 
-  // 3. Convert string inputs to other types
-  floatString := " 3.86O "
-  validConvertedData, err := strconv.ParseFloat(strings.TrimSpace(floatString), 64)
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    fmt.Println("String to Float - ", validConvertedData)
+	// 3. Convert string inputs to other types
+	floatString := " 3.86O "
+	validConvertedData, err := strconv.ParseFloat(strings.TrimSpace(floatString), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("String to Float - ", validConvertedData)
+	}
+
+	// 4. Convert types before using
+	var a int = 8
+	var b float64 = 42.75
+	ab0 := float64(a) + b
+	ab1 := a + int(b)
+	fmt.Printf("(float64) Total - %v, Type - %T\n", ab0, ab0)
+	fmt.Printf("(int) Total - %v, Type - %T\n", ab1, ab1)
+
+	// 5. Use the math package
+	c1, c2, c3 := 23.5, 65.1, 76.3
+	c0 := c1 + c2 + c3
+	fmt.Println(c0)
+
+	c0 = math.Round(c0*100) / 100
+	fmt.Println(c0)
+
+	circleRadius := 15.5
+	circumference := circleRadius * 2 * math.Pi
+	fmt.Printf("Circumference - %.2f\n", circumference)
+
+	// 6. Work with dates and times
+	n := time.Now()
+	fmt.Println("Date & Time - ", n.Format(time.ANSIC))
+
+	// 7. How memory is allocated & managed
+	student := make(map[string]int)
+	student["history"] = 35
+	student["biology"] = 75
+	fmt.Println("Student -", student)
+
+	// 8. Reference values with pointers
+	var d int = 42
+	var pointer *int = &d
+	fmt.Println("Before value & pointer - ", d, *pointer)
+	*pointer = 31
+	fmt.Println("After value & pointer - ", d, *pointer)
+
+	// 9. Store ordered values in arrays
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+
+	var numbers = [3]int{105, 425}
+	fmt.Println(numbers, len(numbers))
+
+	// 10. Manage ordered values in slices
+	var colours = []string{"Red", "Green", "Blue"}
+	colours = append(colours, "White")
+	fmt.Println(colours)
+
+	// Remove 1st & last item from slice
+	colours = append(colours[1 : len(colours)-1])
+	fmt.Println(colours)
+
+	numbersSlice := make([]int, 5)
+	numbersSlice[0] = 32
+	numbersSlice[1] = 43
+	numbersSlice[2] = 76
+	numbersSlice[3] = 21
+	numbersSlice[4] = 87
+	numbersSlice = append(numbersSlice, 13)
+	fmt.Println("Before sort -", numbersSlice)
+	sort.Ints(numbersSlice)
+	fmt.Println("After sort -", numbersSlice)
+
+	// 11. Store unordered values in maps
+	states := make(map[string]string)
+	fmt.Println(states)
+	states["BC"] = "British Columbia"
+	states["ON"] = "Ontario"
+	states["SK"] = "Saskatchwan"
+  for key, val := range states {
+    fmt.Printf("%v: %v\n", key, val)
   }
+	delete(states, "SK")
+	fmt.Println(states)
 
-  // 4. Convert types before using
-  var a int = 8
-  var b float64 = 42.75
-  ab0 := float64(a) + b
-  ab1 := a + int(b)
-  fmt.Printf("(float64) Total - %v, Type - %T\n", ab0, ab0)
-  fmt.Printf("(int) Total - %v, Type - %T\n", ab1, ab1)
-
-  // 5. Use the math package
-  c1, c2, c3 := 23.5, 65.1, 76.3
-  c0 := c1 + c2 + c3
-  fmt.Println(c0)
-
-  c0 = math.Round(c0 * 100) / 100
-  fmt.Println(c0)
-
-  circleRadius := 15.5
-  circumference := circleRadius * 2 * math.Pi
-  fmt.Printf("Circumference - %.2f\n", circumference)
-
-  // 6. Work with dates and times
-  n := time.Now()
-  fmt.Println("Date & Time - ", n.Format(time.ANSIC))
-
-  // 7. How memory is allocated & managed
-  student := make(map[string]int)
-  student["history"] = 35
-  student["biology"] = 75
-  fmt.Println("Student -", student)
-
-  // 8. Reference values with pointers
-  var d int = 42
-  var pointer *int = &d
-  fmt.Println("Before value & pointer - ", d, *pointer)
-  *pointer = 31
-  fmt.Println("After value & pointer - ", d, *pointer)
-
-  // 9. Store ordered values in arrays
-  var colors [3]string;
-  colors[0] = "Red"
-  colors[1] = "Green"
-  colors[2] = "Blue"
-  fmt.Println(colors)
-
-  var numbers = [3]int{105, 425};
-  fmt.Println(numbers, len(numbers))
-
-  // 10. Manage ordered values in slices
-  var colours = []string{"Red", "Green", "Blue"};
-  colours = append(colours, "White")
-  fmt.Println(colours)
-
-  // Remove 1st & last item from slice
-  colours = append(colours[1:len(colours) - 1])
-  fmt.Println(colours)
-
-  numbersSlice := make([]int, 5)
-  numbersSlice[0] = 32
-  numbersSlice[1] = 43
-  numbersSlice[2] = 76
-  numbersSlice[3] = 21
-  numbersSlice[4] = 87
-  numbersSlice = append(numbersSlice, 13)
-  fmt.Println("Before sort -", numbersSlice)
-  sort.Ints(numbersSlice)
-  fmt.Println("After sort -", numbersSlice)
-
-  sum := 0
-  for i := 0; i < 10; i++ {
-    sum += i
-  }
-  fmt.Println(sum)
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
